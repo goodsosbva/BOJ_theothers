976423c34fbfdc4e62efde7ef99c803b997825e5
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2024-08-02 09:54:53 +0900

    Add files via upload

--- 헌진시치가낸문제5.py
+++ 헌진시치가낸문제5.py
@@ -1,3 +1,16 @@
+'''
+3번 문제
+정수배열 nums와 정수 k, t 가 주어졌을때 아래 조건을 만족하면 임의의 서로다른 정수 i,j가 존재하면  True, 아니면 False
+
+조건
+
+abs(nums[i] - nums[j]) <= t 
+abs(i-j) <= k 
+
+0 < nums.length() < 10**5+1
+0< nums[i] < 10**5+1(수정됨
+'''
+
 def answer(nums, k, t):
     if k < 1 or t < 0:
         return False
